edfd32aa5d8715022e136f3054662fd909e01c79
ismailasega <asega03@gmail.com>
2022-11-01 03:14:47 +0300

    desktop-view creation

--- src/components/ShowDetails.vue
+++ src/components/ShowDetails.vue
@@ -25,22 +25,103 @@ const closeShowDetailsModal = inject("closeShowDetailsModal");
 </style>
 <template>
   <!-- Desktop View -->
-  <div class="top-0 desktop-view detailsModal px-4 block fixed z-30 w-full bg-[#181818] h-20" v-if="isShowDetailsModal">
-    <div class="flex items-center justify-between border-b border-gray-400 p-4">
-      <div class="text-2xl font-bold">{{selectedShowDetails.name}}</div>
-      <XCircleIcon @click="closeShowDetailsModal" class="h-10 w-10 hover:cursor-pointer hover:text-lime-600" />
-    </div>
-    <div class="mt-6 p-4 relative flex flex-row flex-1 overflow-auto ">
-      <div class="w-1/2">
-        <img :src="selectedShowDetails?.image?.original"
-          class="rounded-lg w-[500px] flex items-center justify-center" />
+  <div
+    class="top-0 desktop-view detailsModal pb-10 block fixed overflow-x-hidden overflow-y-auto z-30 w-full bg-[#181818] h-20"
+    v-if="isShowDetailsModal">
+    <div class="w-[1039px] m-auto ">
+      <div class="absolute -top-20">
+        <div
+          class="flex items-center z-50 fixed bg-[#181818] shadow-md w-[1039px] justify-between border-b border-gray-600 p-2">
+          <div class="text-2xl font-medium text-gray-200">{{ selectedShowDetails?.name }}</div>
+          <XCircleIcon @click="closeShowDetailsModal" class="h-6 w-6 hover:cursor-pointer hover:text-white" />
+        </div>
       </div>
-      <div class="w-1/2">
+      <div class="mt-20 p-4 flex flex-row">
+        <div class="w-1/2">
+          <img :src="selectedShowDetails?.image?.original"
+            class="rounded-lg m-auto w-[300px] flex items-center justify-center" />
+        </div>
+        <div class="w-1/2 px-12">
+          <div class="bg-black bg-opacity-30 p-4 rounded-lg">
+            <h6 class="text-gray-200 font-medium text-md">Genres</h6>
+            <div class="flex flex-row gap-1">
+              <span v-for="(genre, index) in selectedShowDetails?.genres" :key="index">
+                <span class="text-sm ">{{ genre + ',' }}</span>
+              </span>
+            </div>
+            <div class="flex-row flex justify-between items-center pt-3 mt-2">
+              <span class="bg-slate-600 rounded-md py-1 px-4 text-sm ">{{ selectedShowDetails?.type }}</span>
+              <span class="bg-blue-600 rounded-md py-1 px-4 text-sm ">{{ selectedShowDetails?.runtime + ' '
+                  + 'mins'
+              }}</span>
+              <span class="bg-orange-600 rounded-md py-1 px-4 text-sm flex flex-row items-center "><span>{{
+                  selectedShowDetails?.rating?.average
+              }}</span>
+                <StarIcon class="ml-1 h-4 w-4" />
+              </span>
+            </div>
+            <div class="flex flex-row items-center justify-between mt-5 border-b border-gray-700 pb-4">
+              <div>
+                <h6 class="text-gray-200 font-medium text-md">Language</h6>
+                <span class="text-sm">{{ selectedShowDetails?.language }}</span>
+              </div>
+              <div>
+                <h6 class="text-gray-200 font-medium text-md">Premiered</h6>
+                <span class="text-sm">{{ selectedShowDetails?.premiered }}</span>
+              </div>
+              <div>
+                <h6 class="text-gray-200 font-medium text-md">Status</h6>
+                <span class="text-sm">{{ selectedShowDetails?.status }}</span>
+              </div>
+            </div>
+          </div>
+          <div class="py-7 px-4">
+            <div class="pb-4">
+              <h6 class="text-gray-200 font-medium text-md">Schedule</h6>
+              <div class="grid grid-cols-5 gap-2 text-sm justify-between">
+                <div v-for="(schedule, index) in selectedShowDetails?.schedule?.days" :key="index">
+                  <span>{{ schedule }}</span>
+                </div>
+              </div>
+              <span class="text-sm">Time: {{ selectedShowDetails?.schedule?.time }}</span>
+            </div>
+            <div class=" mt-2 pb-4 flex flex-row items-center">
+              <div class=" border-r border-gray-600 pr-5 ">
+                <h6 class="text-gray-200 font-medium text-md">Network</h6>
+                <div class="flex flex-col">
+                  <span class="text-sm">{{ selectedShowDetails?.network?.name }}</span>
+                  <span class="text-sm">Country: {{ selectedShowDetails?.network?.country?.name }}</span>
+                  <span class="text-sm">Timezone: {{ selectedShowDetails?.network?.country?.timezone }}</span>
+
+                </div>
+              </div>
+              <div class="pl-5">
+                <h6 class="text-gray-200 font-medium text-md">Web Channel</h6>
+                <div class="flex flex-col">
+                  <span class="text-sm">{{ selectedShowDetails?.webChannel?.name }}</span>
+                  <span class="text-sm">Country: {{ selectedShowDetails?.webChannel?.country }}</span>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="p-5 bg-black bg-opacity-30 rounded-xl">
+        <div class="mt-2 pb-4">
+          <h6 class="text-gray-200 font-medium text-md">Summary</h6>
+          <div class="text-sm" v-html="selectedShowDetails?.summary"> </div>
+        </div>
+        <div class="mt-2">
+          <h6 class="text-gray-200 font-medium text-md">Official site</h6>
+          <a :href="selectedShowDetails?.officialSite" class="text-sm">{{ selectedShowDetails?.officialSite }}</a>
+        </div>
       </div>
     </div>
   </div>
   <!-- Mobile View -->
-  <div class="top-0 mobile-view detailsModal block overflow-y-auto pb-10 fixed z-30 w-full bg-[#181818] h-20" v-if="isShowDetailsModal">
+  <div class="top-0 mobile-view detailsModal block overflow-y-auto pb-10 fixed z-30 w-full bg-[#181818] h-20"
+    v-if="isShowDetailsModal">
     <div class="relative">
       <img :src="selectedShowDetails?.image?.original"
         class="rounded-lg opacity-70 w-[500px] flex items-center justify-center" />
@@ -48,19 +129,21 @@ const closeShowDetailsModal = inject("closeShowDetailsModal");
       <div class="m-10 ">
         <div
           class="bg-white bg-opacity-20 flex flex-col z-40 backdrop-blur-sm drop-shadow-lg h-[150px] rounded-lg absolute bottom-0 p-2 w-full text-white">
-          <span class="font-bold text-xl mb-4">{{ selectedShowDetails.name }}</span>
+          <span class="font-bold text-xl mb-4">{{ selectedShowDetails?.name }}</span>
           <div class="flex flex-col px-4">
             <div class="flex flex-wrap jitems-center justify-between">
-              <span v-for="(genre, index) in selectedShowDetails.genres" :key="index">
-                <span class="bg-pink-600 justify-self-center rounded-full py-1 px-4 text-sm ">{{genre}}</span>
+              <span v-for="(genre, index) in selectedShowDetails?.genres" :key="index">
+                <span class="bg-pink-600 justify-self-center rounded-md py-1 px-4 text-sm ">{{ genre }}</span>
               </span>
             </div>
             <div class="flex-row flex justify-between items-center border-t boder-gray-600 pt-3 mt-4">
-              <span class="bg-slate-600 rounded-full py-1 px-4 text-sm ">{{selectedShowDetails.type}}</span>
-              <span class="bg-blue-600 rounded-full py-1 px-4 text-sm ">{{selectedShowDetails.runtime + ' '
-                +'mins'}}</span>
-              <span
-                class="bg-orange-600 rounded-full py-1 px-4 text-sm flex flex-row items-center "><span>{{selectedShowDetails.rating.average}}</span>
+              <span class="bg-slate-600 rounded-md py-1 px-4 text-sm ">{{ selectedShowDetails?.type }}</span>
+              <span class="bg-blue-600 rounded-md py-1 px-4 text-sm ">{{ selectedShowDetails?.runtime + ' '
+                  + 'mins'
+              }}</span>
+              <span class="bg-orange-600 rounded-md py-1 px-4 text-sm flex flex-row items-center "><span>{{
+                  selectedShowDetails?.rating?.average
+              }}</span>
                 <StarIcon class="ml-1 h-4 w-4" />
               </span>
             </div>
@@ -76,42 +159,45 @@ const closeShowDetailsModal = inject("closeShowDetailsModal");
       <div class="flex flex-row items-center justify-between border-b border-gray-700 pb-4">
         <div>
           <h6 class="text-gray-200 font-medium text-md">Language</h6>
-          <span class="text-sm">{{selectedShowDetails.language}}</span>
+          <span class="text-sm">{{ selectedShowDetails?.language }}</span>
         </div>
         <div>
           <h6 class="text-gray-200 font-medium text-md">Premiered</h6>
-          <span class="text-sm">{{selectedShowDetails.premiered}}</span>
+          <span class="text-sm">{{ selectedShowDetails?.premiered }}</span>
         </div>
         <div>
           <h6 class="text-gray-200 font-medium text-md">Status</h6>
-          <span class="text-sm">{{selectedShowDetails.status}}</span>
+          <span class="text-sm">{{ selectedShowDetails?.status }}</span>
         </div>
       </div>
       <div class="mt-2 border-b border-gray-700 pb-4">
         <h6 class="text-gray-200 font-medium text-md">Schedule</h6>
         <div class="grid grid-cols-5 gap-2 text-sm justify-between">
-          <div v-for="(schedule, index) in selectedShowDetails.schedule.days" :key="index">
-            <span>{{schedule}}</span>
+          <div v-for="(schedule, index) in selectedShowDetails?.schedule?.days" :key="index">
+            <span>{{ schedule }}</span>
           </div>
         </div>
-        <span class="text-sm">Time: {{selectedShowDetails.schedule.time}}</span>
+        <span class="text-sm">Time: {{ selectedShowDetails?.schedule?.time }}</span>
       </div>
-      <div class="mt-2 border-b border-gray-700 pb-4">
+      <div class="mt-2  pb-4">
         <h6 class="text-gray-200 font-medium text-md">Network</h6>
         <div class="flex flex-col">
-          <span class="text-sm">{{selectedShowDetails.network.name}}</span>
-        <span class="text-sm">Country: {{selectedShowDetails.network.country.name}}</span>
-        <span class="text-sm">Timezone: {{selectedShowDetails.network.country.timezone}}</span>
+          <span class="text-sm">{{ selectedShowDetails.network?.name }}</span>
+          <span class="text-sm">Country: {{ selectedShowDetails?.network?.country?.name }}</span>
+          <span class="text-sm">Timezone: {{ selectedShowDetails?.network?.country?.timezone }}</span>
 
         </div>
       </div>
-      <div class="mt-2 pb-4">
-        <h6 class="text-gray-200 font-medium text-md">About</h6>
-        <div class="text-sm" v-html="selectedShowDetails.summary"> </div>
-      </div>
-      <div class="mt-2">
-        <h6 class="text-gray-200 font-medium text-md">Official site</h6>
-        <a :href="selectedShowDetails.officialSite" class="text-sm">{{selectedShowDetails.officialSite}}</a>
+      <div class="p-4 bg-black bg-opacity-30 rounded-xl">
+        <div class="pb-4">
+          <h6 class="text-gray-200 font-medium text-md">Summary</h6>
+          <div class="text-sm" v-html="selectedShowDetails?.summary"> </div>
+        </div>
+        <div class="mt-2 border-b border-gray-700 pb-4">
+          <h6 class="text-gray-200 font-medium text-md">Official site</h6>
+          <a :href="selectedShowDetails?.officialSite" class="text-sm">{{ selectedShowDetails?.officialSite }}</a>
+        </div>
+
       </div>
     </div>
   </div>
