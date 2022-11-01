f791d014945b93f6dbbd57b0c87a8107a9738af3
Asega Debele Ismail <asega03@gmail.com>
2022-11-01 18:05:48 +0300

    unit testing pinia store done

--- src/tests/TvShows.spec.js
+++ src/tests/TvShows.spec.js
@@ -25,21 +25,21 @@ describe('Should be able to check array state and return data', () => {
      * creating a fresh pinia and making it active so as it is automatically picked
      * up by any useShowsStore() call without having to pass it to it: `useShowsStore(pinia)`
      */
-    setActivePinia(createPinia())
+    setActivePinia(createPinia());
   })
 
   it.concurrent('Should returns Tv Shows', async () => {
     const tvShowsList = useShowsStore();
     expect(tvShowsList.shows).toEqual([]);
-    await tvShowsList.getAllTvShows()
-    let data = tvShowsList.shows
-    expect(tvShowsList.shows.length > 0).toBeTruthy()
+    await tvShowsList.getAllTvShows();
+    let data = tvShowsList.shows;
+    expect(tvShowsList.shows.length > 0).toBeTruthy();
   })
 
   it.concurrent('Should be able to return response', async () => {
-    let searchData = "Kirby Buckets"
+    let searchData = "Kirby Buckets";
     const tvShowsList = useShowsStore();
-    await tvShowsList.searchShowsByName(searchData)
-    expect(tvShowsList.shows.length >= 1).toBeTruthy()
+    await tvShowsList.searchShowsByName(searchData);
+    expect(tvShowsList.shows.length >= 1).toBeTruthy();
   })
 })
